508da74468099bd22e49f14d1bbf0c2016d74822
thdmd9 <63652571+Knowsoeun@users.noreply.github.com>
2022-04-13 21:09:45 +0900

    Update revsqueeze-자바.java

--- 20220413/실습1/revsqueeze-자바.java
+++ 20220413/실습1/revsqueeze-자바.java
@@ -1,36 +1,40 @@
-package prac;
+
 
 import java.util.Scanner;
 
-public class prac_for_each {
+public class revsqueeze {
 
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("문자열\n문자"); //입력
-		Scanner sc=new Scanner(System.in); 
-		String s=sc.nextLine();
-		char c=sc.next().charAt(0);
-		// 문자열 0번째 문자를 char타입으로 변환해준다. (문자)
+		//문자열, 문자 입력받기
+		System.out.println("문자열\n문자");    
+		Scanner sc = new Scanner(System.in); 
+		String s = sc.nextLine();     //문자열
+		char c = sc.next().charAt(0); //Scanner를 이용한 char 입력
+		/*
+		문자열 0번째 문자를 char타입으로 변환해준다. (문자)
+		아래와 똑같이 쓸 수도 있다.
+		String s = sc.next();
+		char c = s.charAt(0); 
+		*/
 		
 		// Sting s="abcabc"; char c='a'
 	
 		
-		String result=revsqueeze(s,c);
-		System.out.println(result);
+		String result = revsqueeze(s,c); //함수 호출해서 result에 저장
+		System.out.println(result);      //result 출력
 
 	}
 	
 	/*for문*/
-	public static String revsqueeze(String s,char c) {
+	public static String revsqueeze(String s, char c) {
 		
-		String ss=s;
-		char cc=c;
-		String s_result=""; //반환 문자열
+		String s_result = ""; //반환 문자열
 		
 		// 문자열 길이만큼 뒤에서부터 역순으로 c와 비교해서 c가 아니면 붙여준다.
-		for (int i=0; i<ss.length(); i++) {  //문자열 길이만큼 반복
+		for (int i=0; i<s.length(); i++) {  //문자열 길이만큼 반복
 			if (s.charAt(s.length()-i-1)!=c) {  //뒤에서부터 문자를 하나하나 c와 비교해서 같지 않으면
 				s_result=s_result.concat(Character.toString(s.charAt(s.length()-i-1))); //반환 문자열에 해당 문자를 붙여준다.
 			}
