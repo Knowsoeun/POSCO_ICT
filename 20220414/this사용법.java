0a9b62ddf376f72a5a52050afb7dc932b799a678
thdmd9 <63652571+Knowsoeun@users.noreply.github.com>
2022-04-15 10:01:45 +0900

    Create this사용법.java

---
+++ 20220414/this사용법.java
@@ -0,0 +1,15 @@
+public class Tr{
+	private String a;
+	private String b;
+
+
+	//아래 두개는 똑같은 뜻!
+	public Tr(String a, String b){
+		a=x; //위 String a에 들어간다.
+		b=x; //위 String b에 들어간다.
+	}
+	public Tr(String a, String b){
+		this.a=a;
+		this.b=b;
+	}
+}
