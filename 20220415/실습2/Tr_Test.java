8e1da66d17bcc22b859e959e48c7a567b1fe0b6f
thdmd9 <63652571+Knowsoeun@users.noreply.github.com>
2022-04-15 12:34:50 +0900

    Update Tr_Test.java

--- 20220415/실습2/Tr_Test.java
+++ 20220415/실습2/Tr_Test.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Tr_Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 문장 2개를 입력받는다.
 		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
 		String t = sc.nextLine();
@@ -15,10 +15,10 @@ public class Tr_Test {
 		Ti Q;
 		
 		P = new Select2(s,t);
-		Q=((Select2)P).check();
+		Q=((Select2)P).check(); //Q는 부모 인터페이스 Ti-> Select2 자식 클래스로 캐스팅 뒤 체크함수에 s,t를 적용한 P
 		
 		
-		System.out.println(Q.cmp());
+		System.out.println(Q.cmp()); //cmp함수 결과 출력
 		
 	}
 
