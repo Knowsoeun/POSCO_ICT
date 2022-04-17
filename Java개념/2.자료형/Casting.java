0c494626ce28e2c4c876b51a20b3ccb7ef04459d
thdmd9 <63652571+Knowsoeun@users.noreply.github.com>
2022-04-17 17:55:22 +0900

    Update Casting.java

--- Java개념/2.자료형/Casting.java
+++ Java개념/2.자료형/Casting.java
@@ -2,12 +2,17 @@ public class Test{
   public static void main(String[] args){
     int a = 10;
     double b;
-    b = a;  System.out.println(b);  
-    a = b;  System.out.println(a);  // 큰 자료형(double) -> 작은 자료형(int) 오버플로우로 error 발생
-    a = (int)b;  System.out.println(a); //명시적 형변환으로 강제 형변환하면 error 발생x
-    b = 10/3;  System.out.println(b);
-    b = (double)10/3;  System.out.println(b);
-    b = (int)10.20 + (int)3.3;  System.out.println(b);
-    b = (int)10.2 + 3.3;  System.out.println(b);
+    b = a;  System.out.println(b);  //10 .0
+    
+    // 큰 자료형(double) -> 작은 자료형(int) 오버플로우로 error 발생
+    a = b;  System.out.println(a); 
+    
+    //명시적 형변환으로 강제 형변환하면 error 발생x
+    a = (int)b;  System.out.println(a);
+    
+    b = 10/3;  System.out.println(b);  //3.333333
+    b = (double)10/3;  System.out.println(b);  //3.3333333
+    b = (int)10.20 + (int)3.3;  System.out.println(b);  //13.0
+    b = (int)10.2 + 3.3;  System.out.println(b);  //13.3
   }
 }
