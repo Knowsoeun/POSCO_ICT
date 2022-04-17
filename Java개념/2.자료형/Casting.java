5841569d90365f294b520d12d34b8a3ec5af0ae0
thdmd9 <63652571+Knowsoeun@users.noreply.github.com>
2022-04-17 17:59:23 +0900

    Update Casting.java

--- Java개념/2.자료형/Casting.java
+++ Java개념/2.자료형/Casting.java
@@ -11,8 +11,11 @@ public class Test{
     //명시적 형변환으로 강제 형변환하면 error 발생x
     a = (int)b;  System.out.println(a); //10
     
+    //double안붙이면 소수점 첫번째까지 나오고, double붙이면 끝까지 나온다.
     b = 10/3;  System.out.println(b);  //3.0
     b = (double)10/3;  System.out.println(b);  //3.3333333
+    
+    
     b = (int)10.20 + (int)3.3;  System.out.println(b);  //13.0
     b = (int)10.2 + 3.3;  System.out.println(b);  //13.3
   }
