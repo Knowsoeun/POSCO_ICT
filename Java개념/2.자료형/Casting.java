daba95c95cd44a4da5e05dee9e39125342d0d242
thdmd9 <63652571+Knowsoeun@users.noreply.github.com>
2022-04-17 17:56:03 +0900

    Update Casting.java

--- Java개념/2.자료형/Casting.java
+++ Java개념/2.자료형/Casting.java
@@ -8,9 +8,9 @@ public class Test{
     a = b;  System.out.println(a); 
     
     //명시적 형변환으로 강제 형변환하면 error 발생x
-    a = (int)b;  System.out.println(a);
+    a = (int)b;  System.out.println(a); //10
     
-    b = 10/3;  System.out.println(b);  //3.333333
+    b = 10/3;  System.out.println(b);  //3.0
     b = (double)10/3;  System.out.println(b);  //3.3333333
     b = (int)10.20 + (int)3.3;  System.out.println(b);  //13.0
     b = (int)10.2 + 3.3;  System.out.println(b);  //13.3
