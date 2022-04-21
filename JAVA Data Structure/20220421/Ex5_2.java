b1f66ac222e3dc7c8bde097bb40c2e69fe7aba89
thdmd9 <63652571+Knowsoeun@users.noreply.github.com>
2022-04-21 12:33:24 +0900

    Create Ex5_2.java

---
+++ JAVA Data Structure/20220421/Ex5_2.java
@@ -0,0 +1,18 @@
+package prac6;
+
+public class Ex5_2 {
+
+	public static void main(String srgs[]){ 
+		int sale[][][] = new int [][][]{{{63, 84, 140, 130}, 
+			{157, 209, 251, 312}}, {{59, 80, 135}, {149, 187, 239, 310}} }; 
+		for(int i=0; i<2; i++){ 
+			System.out.printf("<< %d 팀 >> %n", i+1); 
+			for(int j=0; j<2; j++){ 
+				for(int k=0; k<4; k++) 
+					System.out.printf("%d/4분기 : sale[%d][%d] = %d %n", k+1, i, j, k, sale[i][j][k]); 
+					System.out.println("-------------"); 
+				}
+		}
+	}
+
+}
