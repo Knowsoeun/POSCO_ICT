56d208f4b25c24c52d72b224aca6067d6568c2cb
thdmd9 <63652571+Knowsoeun@users.noreply.github.com>
2022-04-27 14:47:52 +0900

    Update cmd창에서 실행.sql

--- MySQL/20220427/cmd창에서 실행.sql
+++ MySQL/20220427/cmd창에서 실행.sql
@@ -3,6 +3,6 @@ mysql -u root -p
 use web3;
 show tables;
 
-create user 'uesr1'%identified by '1234';  // %: 어디서든 접속 가능
+-- create user 'uesr1'%identified by '1234';  // %: 어디서든 접속 가능
 create user 'uesr1'@ identified by '1234'; // @: local에서 접속 가능
 
