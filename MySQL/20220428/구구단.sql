ac0afef97ce48c6c5e017c483eebbc476fae9093
thdmd9 <63652571+Knowsoeun@users.noreply.github.com>
2022-04-28 15:46:26 +0900

    Update 구구단.sql

--- MySQL/20220428/구구단.sql
+++ MySQL/20220428/구구단.sql
@@ -12,9 +12,9 @@ BEGIN                                      -- 시작
     WHILE (i < 10) DO                      -- 2~9까지 곱함
         SET k = 1;                         -- 1~9 까지
         WHILE (k < 10) DO                  -- 1~9까지 돈다.
-			SET str = '';                        -- str은 처음에 공백으로    
+			SET str = '';                           -- str은 처음에 공백으로    
             SET str = CONCAT(str, ' ', i, 'x', k, '=', i * k);  -- str(공백 i X k = i*k) 로 출력될거 공백은 띄어쓰기용
-            INSERT INTO pbj_db.guguTBL VALUES(str); -- 2*1 부터 gugutbl에 밀어넣자
+            INSERT INTO pbj_db.guguTBL VALUES(str);             -- 2*1 부터 gugutbl에 밀어넣자
             SET k = k + 1;                 -- k값 증가
 		END WHILE;                             -- 1~9 까지의 while 끝
         SET i = i + 1;                     -- 1~9까지 돌았으니 i단 증가
