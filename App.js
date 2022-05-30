e5278fea6e94421a4d1e3be68780e66807aa9cf4
thdmd9 <63652571+Knowsoeun@users.noreply.github.com>
2022-05-30 15:39:01 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,43 +1,42 @@
-import "./App.css"
-import Hello from "./component/Hello";
-import Hi from "./component/Hi";
-import Title from "./component/Title";
-import Count from "./component/Count";
-import Clock from "./component/Clock";
-import Form from "./component/Form";
-import SignupForm from "./component/SignupForm";
-import First from "./component/First";
-import { useState } from "react";
+import './App.css';
+import { useState } from 'react';
+let count = 0;
+let index = 0;
+function App() {
+  const [students, setStudents] =useState(new Array(20).fill(1).map((data,i)=>{
+    return i+1
+  })); 
+  const [teams,setTeams] = useState([[],[],[],[],[]]);
+  const max = 4;
 
-
-
-function App(){
-  /* const [time, setTime]=useState(new Date().toLocaleString());
-  setInterval(() => {
-    setTime(new Date().toLocaleString());
-  }, 1000); */
-
+  const pickStudentRandom = ()=>{
+    const randomStudent = parseInt(Math.random()*students.length-1);
+    pickStudent(students[randomStudent])
+  }
+  const pickStudent =(i)=>{
+    setStudents(students.filter(data=>data!==i));
+    if(count===max) {
+      index = index+1;
+      count = 0;
+    } 
+    const newTeams = [...teams];
+    newTeams[index].push(i);
+    setTeams(newTeams)
+    count = count+1;
+  } 
   return (
     <div className="App">
-      <header className="App-header">
-        <First></First>
-        <Vote></Vote>
-        {/* <Form></Form>
-        <SignupForm></SignupForm> */}
-        {/* <Hi></Hi>
-        <Hello></Hello>
-        <Title title ="hello" name="react">
-          <Title title="pppp" name="aaaa"></Title>
-        </Title>
-        <Title title = "hi" name="reactt1"></Title>
-        <Count></Count>
-        <Clock time={time}></Clock> */}
-      </header>
+      <table>
+        {teams.map((team,i)=>(
+        <tr>
+          <td>{team.map(student=>`${student},`)}</td>
+        </tr>
+        ))}
+        
+      </table>
+          <button onClick={pickStudentRandom}>추첨</button>
     </div>
   );
 }
-
-
-
 
 export default App;
